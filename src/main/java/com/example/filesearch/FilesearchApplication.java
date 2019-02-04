88fdfa59f633e89a2433860579aaa7708186fee6
shonorov <shonorov@gmail.com>
2019-02-04 16:29:46 +0300

    first commit

---
+++ src/main/java/com/example/filesearch/FilesearchApplication.java
@@ -0,0 +1,14 @@
+package com.example.filesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesearchApplication.class, args);
+    }
+
+}
+
